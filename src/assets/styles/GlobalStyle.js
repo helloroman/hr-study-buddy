5da9cf48a5c9d7b45350a3654c9b656c77679aee
Adam Romanski <adamromanski@MacBook-Pro-Adam.local>
2021-02-01 22:47:54 +0100

    Fix Google fonts bug

--- src/assets/styles/GlobalStyle.js
+++ src/assets/styles/GlobalStyle.js
@@ -1,8 +1,6 @@
 import { createGlobalStyle } from 'styled-components';
 
 export const GlobalStyle = createGlobalStyle`
-  @import url('https://fonts.googleapis.com/css2?family=Montserrat:ital,wght@0,700;1,400&display=swap');
-        
   html {
     box-sizing: border-box;
   }
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from 'views/Root';
+import 'assets/styles/fonts.css';
 
 ReactDOM.render(
   <React.StrictMode>
