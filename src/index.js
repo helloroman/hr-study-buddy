3920a998e7101f031ce7b0bc1839aa34747039c4
Adam Romanski <adamromanski@MacBook-Pro-Adam.local>
2021-04-05 12:59:01 +0200

    Fix firefox issues

--- src/index.js
+++ src/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import Root from 'views/Root';
 import 'assets/styles/fonts.css';
+import { worker } from 'mocks/browser';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <Root />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+worker.start().then(() => {
+  ReactDOM.render(
+    <React.StrictMode>
+      <Root />
+    </React.StrictMode>,
+    document.getElementById('root')
+  );
+});
