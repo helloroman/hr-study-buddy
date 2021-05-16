9e34c26ccc0a4928612b7435d8a56c1bbe65463f
Adam Romanski <adam.romanski@hotjar.com>
2021-05-16 20:49:39 +0200

    Auth hook final

---
+++ src/hooks/useAuth.js
@@ -0,0 +1,57 @@
+import React, { useContext, useEffect, useState } from 'react';
+import axios from 'axios';
+
+const AuthContext = React.createContext({});
+
+export const AuthProvider = ({ children }) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const token = localStorage.getItem('token');
+    if (token) {
+      (async () => {
+        try {
+          const response = await axios.get('/me', {
+            headers: {
+              authorization: `Bearer ${token}`,
+            },
+          });
+          setUser(response.data);
+        } catch (e) {
+          console.log(e);
+        }
+      })();
+    }
+  }, []);
+
+  const signIn = async ({ login, password }) => {
+    console.log(login, password);
+    try {
+      const response = await axios.post('/login', {
+        login,
+        password,
+      });
+      setUser(response.data);
+      localStorage.setItem('token', response.data.token);
+    } catch (e) {
+      console.log(e);
+    }
+  };
+
+  const signOut = () => {
+    setUser(null);
+    localStorage.removeItem('token');
+  };
+
+  return <AuthContext.Provider value={{ user, signIn, signOut }}>{children}</AuthContext.Provider>;
+};
+
+export const useAuth = () => {
+  const auth = useContext(AuthContext);
+
+  if (!auth) {
+    throw Error('useAuth needs to be used inside AuthContext');
+  }
+
+  return auth;
+};
