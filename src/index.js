becf875d680f722ade2f9d31064eb7999323fc63
Kamil <kamilurbanek22@gmail.com>
2023-09-24 14:52:47 +0200

    added in package.json openssl legacy provider bcs project wont work without that line

--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'index.css';
-import App from 'views/App';
+import 'index.css'
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from 'views/App'
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>,
+    document.getElementById('root')
+)
